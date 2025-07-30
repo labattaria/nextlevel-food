c79312fbb3295bb807b76f0810ac9628e8345a26
labattaria <hallucinate322@gmail.com>
2025-07-30 20:01:21 +0300

    update MealsPage component to fetch meals data from db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link';
 import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
 import styles from './page.module.scss';
 
-export default function MealsPage() {
+export default async function MealsPage() {
+    const meals = await getMeals();
+
     return (
         <>
             <header className={styles.header}>
@@ -20,7 +23,7 @@ export default function MealsPage() {
                 </p>
             </header>
             <main className={styles.main}>
-                <MealsGrid meals={[]} />
+                <MealsGrid meals={meals} />
             </main>
         </>
     )

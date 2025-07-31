f58ea5088ba83b97c5eb3a62ec9630aca16f5658
labattaria <hallucinate322@gmail.com>
2025-07-31 16:59:22 +0300

    rename dynamic route folder from [mealsSlug] to [mealSlug]

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import styles from './page.module.scss';
+import { getMeal } from '@/lib/meals';
+
+export default function MealDetailsPage({ params }) {
+    const meal = getMeal(params.mealSlug);
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
+    return (
+        <>
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={styles.creator}>
+                        by <a href={`mailto:${meal.creator_name}`}>{meal.creator}</a>
+                    </p>
+                    <p className={styles.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p className={styles.instructions} dangerouslySetInnerHTML={{
+                    __html: meal.instructions,
+                }}
+                ></p>
+            </main>
+        </>
+    );
+}

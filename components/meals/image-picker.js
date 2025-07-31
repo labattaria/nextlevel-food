3dcf7a73a6eca25d746dc951381b9116842ff199
labattaria <hallucinate322@gmail.com>
2025-07-31 19:24:25 +0300

    refactor ImagePicker component by adding an image preview area

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,19 +1,48 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import styles from './image-picker.module.scss';
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState();
     const imageInput = useRef();
 
     const handlePickClick = () => {
         imageInput.current.click();
     }
 
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+
+        if (!file) {
+            setPickedImage(null);
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        }
+
+        fileReader.readAsDataURL(file);
+    } 
+
     return (
         <div className={styles.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={styles.controls}>
+                <div className={styles.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && (
+                        <Image
+                            src={pickedImage}
+                            alt="The image selected by the user."
+                            fill
+                        />
+                    )}
+                </div>
                 <input
                     className={styles.input}
                     type="file"
@@ -21,6 +50,7 @@ export default function ImagePicker({ label, name }) {
                     accept="image/png, image/jpeg"
                     name={name}
                     ref={imageInput}
+                    onChange={handleImageChange}
                 />
                 <button
                     className={styles.button}

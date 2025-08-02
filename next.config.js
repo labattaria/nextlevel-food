2145f4962d4062e264cb442f0009b66b2d4ef1b5
labattaria <hallucinate322@gmail.com>
2025-08-02 18:42:44 +0300

    update next.js config to allow AWS S3 external image URLs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {};
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'labattaria-nextjs-demo-users-image.s3.eu-north-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};

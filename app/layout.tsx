7bb04246874dab6e523cc1783f9ec0e69228f97c
labattaria <hallucinate322@gmail.com>
2025-08-29 18:40:21 +0300

    add Typescript types to root page, root layout and root not-found

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import MainHeader from '@/components/main-header/main-header';
 import './globals.css';
+import type { Metadata } from 'next';
+import type { ReactNode } from 'react';
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'NextLevel Food',
   description: 'Delicious meals, shared by a food-loving community.',
 };
 
-export default function RootLayout({ children }) {
+interface RootLayoutProps {
+  children: ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body>
